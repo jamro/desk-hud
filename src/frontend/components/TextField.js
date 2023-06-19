96d3a28eab9da170d46addc7409ee143a4966cdf
Krzysztof Jamróz <kjamroz83@gmail.com>
2023-06-19 11:48:07 +0200

    refactor: reduce renders of TextField

--- src/frontend/components/TextField.js
+++ src/frontend/components/TextField.js
@@ -5,9 +5,11 @@ export default class TextField extends PIXI.Text {
     this._progress = 0
     this._origText = String(txt)
     this.anchor.set(0.5)
+    this._changed = true
   }
 
   set progress(v) {
+    this._changed = (this._changed || this._progress !== v)
     this._progress = v
   }
 
@@ -16,7 +18,9 @@ export default class TextField extends PIXI.Text {
   }
 
   set text(v) {
-    this._origText = String(v)
+    const newText = String(v)
+    this._changed = (this._changed || this._origText !== newText)
+    this._origText = newText
   }
 
   get text() {
@@ -25,7 +29,11 @@ export default class TextField extends PIXI.Text {
 
   render(renderer) {
     super.render(renderer)
+    if(!this._changed) {
+      return
+    }
 
     super.text = this._origText.substr(0, Math.round(this._progress*this._origText.length))
+    this._changed = false
   }
 }
