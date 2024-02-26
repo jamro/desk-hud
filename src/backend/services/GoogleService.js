50cf408d160a46bca981072e232f03ae11a37fcb
Krzysztof Jamróz <kjamroz83@gmail.com>
2024-02-26 10:36:14 +0100

    add google token refresh

--- src/backend/services/GoogleService.js
+++ src/backend/services/GoogleService.js
@@ -13,7 +13,16 @@ class GoogleService extends Service {
       client_secret: this.config.getProp('google.clientSecret'),
       refresh_token: this.config.getProp('google.clientToken'),
     })
-    
+
+    // Function to refresh token
+    this.refreshToken = async () => {
+      const { tokens } = await this.auth.refreshAccessToken()
+      this.auth.credentials = tokens
+    }
+
+    // Set interval to refresh token every 50 minutes
+    setInterval(this.refreshToken, 50 * 60 * 1000)
+
   }
 
 }
